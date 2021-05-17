172a196106235b7cecb7e6b171c569e00833d1b7
Hubert Truszewski <01150052@pw.edu.pl>
2021-05-17 23:30:26 +0200

    Correct 5.1

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import PositiveInt
 from sqlalchemy.orm import Session
+from .models2 import Shipper
 
 from . import crud, schemas
 from .database import get_db
@@ -23,13 +24,14 @@ async def get_shippers(db: Session = Depends(get_db)):
     return crud.get_shippers(db)
 
 
-@router.get('/suppliers')
+@router.get('/suppliers', response_model=List[Shipper])
 async def get_suppliers(db: Session = Depends(get_db)):
     return crud.get_all_suppliers(db)
 
 
-@router.get('/suppliers/{id}')
+@router.get('/suppliers/{id}', response_model=Shipper)
 async def get_suppliers_by_id(id: int, db: Session = Depends(get_db)):
     result = crud.get_one_supplier(db, id)
     if result is None:
         raise HTTPException(status_code=404)
+    return result
